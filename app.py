715a8b1b053a2ce79097ca4868dc6fb6496b665e
kumawatsardar007 <84585243+kumawatsardar007@users.noreply.github.com>
2022-08-05 00:56:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
-from flask_ngrok import run_with_ngrok
 import pickle
 
 
 app = Flask(__name__)
 model = pickle.load(open('linearregression1.pkl','rb')) 
-run_with_ngrok(app)
 
 @app.route('/')
 def home():
